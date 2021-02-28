a294618b02904aa229a867e4da0096d2c034713e
yulduz <yulduz83@gmail.com>
2021-02-28 13:37:32 -0600

    handles employee updates

--- src/prompt-user.js
+++ src/prompt-user.js
@@ -84,22 +84,7 @@ const addNewEmployeePrompts = () => {
   });
 };
 
-const deleteEmployeePrompts = () => {
-  return employees.getEmployeeFullNames().then((res) => {
-    let employees = [];
-
-    for (let i = 0; i < res.length; i++) {
-      employees.push(res[i].full_name);
-    }
-    return inquirer.prompt({
-      type: "list",
-      name: "employee",
-      message: `Choose employee to delete`,
-      choices: employees,
-    });
-  });
-};
-
+// Add Employee
 const handleNewEmployee = () => {
   let roleId = null;
   let managerId = null;
@@ -108,7 +93,7 @@ const handleNewEmployee = () => {
       return roles.getRoleIdByName(role).then((res) => {
         roleId = res[0].id;
         return employees.getManagerIdByName(manager).then((res) => {
-          if (!managerId) {
+          if (manager === "No manager") {
             managerId = null;
           } else {
             managerId = res[0].id;
@@ -125,13 +110,136 @@ const handleNewEmployee = () => {
   );
 };
 
+const deleteEmployeePrompts = () => {
+  return employees.getEmployeeFullNames().then((res) => {
+    let employees = [];
+
+    for (let i = 0; i < res.length; i++) {
+      employees.push(res[i].full_name);
+    }
+    return inquirer.prompt({
+      type: "list",
+      name: "employee",
+      message: `Choose employee to delete`,
+      choices: employees,
+    });
+  });
+};
+
+// "Remove Employee"
 const handleRemoveEmployee = () => {
   let employeeId = null;
 
   return deleteEmployeePrompts().then(({ employee }) => {
     return employees.getEmployeeIdByName(employee).then((res) => {
       employeeId = res[0].id;
-      return employees.deleteEmployee(employeeId);
+      return employees.deleteEmployee(employeeId).then(() => {
+        console.log(
+          `${employee} has been successfully deleted from the database`
+        );
+      });
+    });
+  });
+};
+
+const updateEmployeeRolePrompts = () => {
+  return employees.getEmployeeFullNames().then((res) => {
+    let employees = [];
+    let existingRoles = [];
+
+    for (let i = 0; i < res.length; i++) {
+      employees.push(res[i].full_name);
+    }
+    return roles.getRoles().then((res) => {
+      for (let i = 0; i < res.length; i++) {
+        existingRoles.push(res[i].title);
+      }
+      return inquirer.prompt([
+        {
+          type: "list",
+          name: "employee",
+          message: `Choose employee to update`,
+          choices: employees,
+        },
+        {
+          type: "list",
+          name: "role",
+          message: `What is your employee's new role `,
+          choices: existingRoles,
+        },
+      ]);
+    });
+  });
+};
+
+// "Update Employee Role"
+const handleUpdateEmployeeRole = () => {
+  let employeeId = null;
+  let roleId = null;
+
+  return updateEmployeeRolePrompts().then(({ employee, role }) => {
+    return employees.getEmployeeIdByName(employee).then((res) => {
+      employeeId = res[0].id;
+      return roles.getRoleIdByName(role).then((res) => {
+        roleId = res[0].id;
+        return employees.updateEmployeeRole(employeeId, roleId).then(() => {
+          return employees.getEmployee(employeeId);
+        });
+      });
+    });
+  });
+};
+
+const updateEmployeeManagerPrompts = () => {
+  let existingEmployees = [];
+  let existingManagers = [];
+
+  return employees.getEmployeeFullNames().then((res) => {
+    for (let i = 0; i < res.length; i++) {
+      existingEmployees.push(res[i].full_name);
+    }
+    return employees.getManagers().then((res) => {
+      for (let i = 0; i < res.length; i++) {
+        existingManagers.push(res[i].manager_name);
+      }
+      existingManagers.push("No manager");
+      return inquirer.prompt([
+        {
+          type: "list",
+          name: "employee",
+          message: `Choose employee to update`,
+          choices: existingEmployees,
+        },
+        {
+          type: "list",
+          name: "manager",
+          message: `Who is your employee's new manager `,
+          choices: existingManagers,
+        },
+      ]);
+    });
+  });
+};
+//Update Employee Manager
+const handleUpdateEmployeeManager = () => {
+  let employeeId = null;
+  let managerId = null;
+
+  return updateEmployeeManagerPrompts().then(({ employee, manager }) => {
+    return employees.getEmployeeIdByName(employee).then((res) => {
+      employeeId = res[0].id;
+      return employees.getManagerIdByName(manager).then((res) => {
+        if (manager === "No manager") {
+          managerId = null;
+        } else {
+          managerId = res[0].id;
+        }
+        return employees
+          .updateEmployeeManager(employeeId, managerId)
+          .then(() => {
+            return employees.getEmployee(employeeId);
+          });
+      });
     });
   });
 };
@@ -156,6 +264,10 @@ const interactWithUser = () => {
           return handleNewEmployee();
         case "Remove Employee":
           return handleRemoveEmployee();
+        case "Update Employee Role":
+          return handleUpdateEmployeeRole();
+        case "Update Employee Manager":
+          return handleUpdateEmployeeManager();
       }
     })
     .then(() => {
